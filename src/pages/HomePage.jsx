2cb37b72d7aa7723bb2605d498153bbf853b7e02
veronicamagnanimi <veronica.magnanimi@hotmail.it>
2025-01-24 15:05:30 +0100

    feat: add header and milestone 3 wip

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
 
     return (
         <>
-        <section>
-            <h1>Benvenuto nella nostra piattaforma</h1>
-            <p>Qui puoi scegliere e guardare i tuoi film preferiti</p>
+        <div className="container">
+        <section className="my-4 text-center">
+            <h1>Welcome to our platform!</h1>
+            <p>Here you can write a review about the movies and see the reviews of other users</p>
+            <Link to="/movies" className="btn btn-secondary">Watch all movies</Link>
         </section>
+        </div>
         </>
     )
 }
